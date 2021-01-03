a6b43ec94aaadfc096b6fa88067363830a0a6d11
asosunoff <al.sosunoff@gmail.com>
2021-01-03 14:06:49 +0300

    added generic

--- 14/src/app.ts
+++ 14/src/app.ts
@@ -30,3 +30,25 @@ const getObjectValue = <T extends object, V extends keyof T>(obj: T, key: V) => 
 
 /* console.log(getObjectValue({ name: 'Alex' }, 'name')); */
 
+// ====
+
+class Collection<T> {
+    constructor(private _items: T[] = []) {
+
+    }
+
+    add(item: T) {
+        this._items.push(item);
+    }
+
+    remove(item: T) {
+        this._items = this._items.filter(i => i !== item);
+    }
+
+    get items(): T[] {
+        return this._items;
+    }
+}
+
+const items = new Collection(['1', '2']);
+const items_2 = new Collection([1, 3, 4]);
