de7da00836b83522ab094542e97904fb75fb2d17
asosunoff <al.sosunoff@gmail.com>
2021-01-04 15:04:21 +0300

    Added decorator method

--- 14/src/decorators.ts
+++ 14/src/decorators.ts
@@ -8,6 +8,14 @@ function LogField<
     console.log(target, propName, target[propName]);
 }
 
+function LogMethod<
+    T extends object,
+    K extends keyof T,
+    R extends TypedPropertyDescriptor<() => void>>
+    (target: T, propertyKey: K, descriptor: R) {
+    console.log(target, propertyKey, descriptor);
+}
+
 @LogClass
 class ComponentTest {
     @LogField
@@ -15,5 +23,11 @@ class ComponentTest {
 
     constructor(name: string) {
         this.name = name;
+    }
+
+    @LogMethod
+    logName(): void {
+        console.log(`Component Name: ${this.name}`);
+
     }
 }
