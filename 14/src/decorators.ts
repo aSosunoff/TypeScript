785f9c41d872e1f84c246be7618ee3a50c33916a
asosunoff <al.sosunoff@gmail.com>
2021-01-04 15:04:05 +0300

    Added decorator field

--- 14/src/decorators.ts
+++ 14/src/decorators.ts
@@ -2,6 +2,18 @@ function LogClass(constructor: Function) {
     console.log(constructor);
 }
 
+function LogField<
+    T extends object,
+    K extends keyof T>(target: T, propName: K) {
+    console.log(target, propName, target[propName]);
+}
+
 @LogClass
 class ComponentTest {
+    @LogField
+    name: string
+
+    constructor(name: string) {
+        this.name = name;
+    }
 }
